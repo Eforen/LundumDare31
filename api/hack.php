86329f8315c7decae4b1ce52234ba82f30cc65ac
Ariel Lothlorien <git@arielkauai.com>
2014-12-07 16:58:17 -1000

    buggy

---
+++ api/hack.php
@@ -0,0 +1,19 @@
+<?php
+include_once("../core/init.php");
+/**
+ * Created by PhpStorm.
+ * User: Eforen
+ * Date: 12/7/14
+ * Time: 4:27 PM
+ */
+
+$m=rand(-1000, 10000);
+$u=new User();
+if($u->exists()){
+	if($u->money()+$m>0)$u->setMoney($u->money()+$m);
+	else $u->setMoney(0);
+	$u->save();
+	echo "OK|".$m,"|",$u->money();
+}else{
+	echo "NOPE";
+}
